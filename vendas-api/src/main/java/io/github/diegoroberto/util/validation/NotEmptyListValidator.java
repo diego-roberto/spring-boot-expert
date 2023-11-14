0b852bc44ecc2ef34d0d6e6fd7e86d0047b9a749
Diego Roberto <diego-roberto@hotmail.com>
2023-11-13 21:03:16 -0300

    Validações de lista

---
+++ vendas-api/src/main/java/io/github/diegoroberto/util/validation/NotEmptyListValidator.java
@@ -0,0 +1,20 @@
+package io.github.diegoroberto.util.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class NotEmptyListValidator
+        implements ConstraintValidator<NotEmptyList, List> {
+
+    @Override
+    public boolean isValid(List list,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        return list != null && !list.isEmpty();
+    }
+
+    @Override
+    public void initialize( NotEmptyList constraintAnnotation ) {
+        //Empty
+    }
+}
