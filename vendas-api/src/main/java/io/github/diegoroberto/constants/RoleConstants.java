799e73ebceb04b8cfcea0e3f5a250a7302db867d
Diego Roberto <diego-roberto@hotmail.com>
2023-11-18 15:58:58 -0300

    Constantes para lidar com roles de usuário

---
+++ vendas-api/src/main/java/io/github/diegoroberto/constants/RoleConstants.java
@@ -0,0 +1,12 @@
+package io.github.diegoroberto.constants;
+
+
+public class RoleConstants {
+    private RoleConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+
+}
